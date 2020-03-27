a8cbeabc0f3c3657b8dbf34d8f74ed11f945453e
Yago Ramas Muiño <a18yagorm@iessanclemente.net>
2020-03-27 13:02:48 +0100

    Creadas carpetas e ficheiros

---
+++ MiModulo/views/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+	'name': 'Clients',
+	'version': '12.0.1.0.0',
+	'summary': 'Guarda Información del cliente',
+	'category': 'Tools',
+	'author': 'Yago Ramas',
+	'maintainer': '',
+	'company': '',
+	'website': '',
+	'depends': ['base'],
+	'data': [
+        'client_views.xml'
+	],
+	'images': [],
+	'license': 'AGPL-3',
+	'installable': True,
+	'application': False,
+	'auto_install': False,
+}
